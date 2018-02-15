ebf4484293c3b7fd98e1bc7875a131573544284a
adas27 <adas27@sapient.com>
2018-02-15 13:07:12 +0530

    added component structure

--- src/containers/GameEngine.js
+++ src/containers/GameEngine.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 class GameEngine extends Component {
     render() {
         return (
-            <div>
-                <h1>Game Engine</h1>
-            </div>
+            <Row>
+                <Col xs={12} style={{backgroundColor: '#e14081', height: '50px'}}>GE HEADER</Col>
+                <Col xs={12} md={8} style={{backgroundColor: '#689f38', height: '400px'}}>GE QUIZ</Col>
+                <Col xs={12} md={4} style={{backgroundColor: '#303f9f', height: '400px'}}>GE SCORELIST</Col>
+            </Row>
         )
     }
 }
