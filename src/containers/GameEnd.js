26fd92780e5c815dddc1bffc32567cfbcb9e365b
shereej ponmily <sponmily@sapient.com>
2018-02-18 13:06:38 +0530

    Minor flex update for game end page

--- src/containers/GameEnd.js
+++ src/containers/GameEnd.js
@@ -8,7 +8,8 @@ class GameEnd extends Component {
         return (
             <Row>
                 <Col xs={12} style={{backgroundColor: '#e14081', height: '50px'}}>GAME END</Col>
-                <Col xs={12} md={12} style={{height: '400px'}}>
+                <Col xs={12} md={4}></Col>
+                <Col xs={12} md={4} style={{height: '400px'}}>
                         <ScoreCard players={this.props.player.players}/>
                 </Col>
             </Row>
